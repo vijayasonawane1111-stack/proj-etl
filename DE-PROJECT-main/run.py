a714ff6bc402a9d1bc76d15b1728d88700bccbca
vijayasonawane1111-stack <vijayasonawane1111@gmail.com>
2026-04-01 22:17:55 +0530

    Add trusted hosts configuration for Flask app

--- DE-PROJECT-main/run.py
+++ DE-PROJECT-main/run.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,request,redirect,url_for
 
 
 app = Flask(__name__)
+app.config['TRUSTED_HOSTS'] = ['127.0.0.1', 'localhost', '3.236.117.211 ']
 
 
 @app.route('/')
